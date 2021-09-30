64232267ffc091d13fb8a306d8a90e24dcea5292
unknown <bruno.pinheiro.freitas@gmail.com>
2021-09-30 18:57:31 -0300

    comm1700 project

---
+++ COMM1700/SQL/insertData.sql
@@ -0,0 +1,13 @@
+INSERT INTO `iwpcomm1700erp`.`anxiety`
+(`id`,`name`,`description`,`level`)
+VALUES
+(default,'rick','lower anxiety level',1),
+(default,'lunaLovegood','',2),
+(default,'deadpool','',3),
+(default,'shaggy','',4),
+(default,'finn','normal anxiety level',5),
+(default,'charlieBrown','',6),
+(default,'spongebob','',7),
+(default,'ironMan','',8),
+(default,'piglet','',9),
+(default,'courageCowardDog','highest anxiety level',10);
